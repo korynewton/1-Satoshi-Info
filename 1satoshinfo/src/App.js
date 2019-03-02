b17fdd4186c68da4dc6edfde8d05a64e152cdd51
korynewton <korynewtondev@gmail.com>
2019-03-02 11:51:31 -0800

    components added

--- 1satoshinfo/src/App.js
+++ 1satoshinfo/src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
+
 import './App.css';
 
+import {endpoint} from './config'
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import PriceList from './components/PriceList';
+
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      priceData : []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(endpoint)
+    .then((res) => console.log(res.data))
+    .catch((err) => console.log(err))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Header />
+        <SearchBar />
+        <PriceList />
+      </>
     );
   }
 }
---
+++ 1satoshinfo/src/components/PriceList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PriceList = () => {
+  return (
+    <div>
+      <h1>Here is my price list</h1>
+    </div>
+  )
+}
+
+export default PriceList
